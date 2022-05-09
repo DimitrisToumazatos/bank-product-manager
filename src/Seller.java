d34078c81af0ba282a55b82181610b1907a95b35
DimitrisToumazatos <di.toumazatos@gmail.com>
2022-05-09 19:39:43 +0300

    add Seller class and add creator to README

---
+++ src/Seller.java
@@ -0,0 +1,22 @@
+Class Seller {
+
+    private String ID;
+    private String firstName;
+    private String lastName;
+    private String TIN;
+
+    Seller(String ID, String firstName, String lastName, String TIN) {
+        this.ID = ID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.TIN = TIN;
+    }
+
+    String getID() {
+        return ID;
+    }
+    
+    public String toString() {
+        return "ID Number: "+this.ID+"\nLast Name: "+this.lastName+"\nFirst Name: "+this.firstName;
+    }
+}
